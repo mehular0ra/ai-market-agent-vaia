29a51f24e153aac37854eaaab275477a933c23b7
Mehul Arora <22mehularora22@gmail.com>
2025-11-03 18:42:18 +0530

    [feat] add embedding service

---
+++ app/services/embedding.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+
+
+class EmbeddingService:
+    def __init__(self):
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        self.model = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
+
+    def generate_embedding(self, text: str) -> list[float]:
+        response = self.client.embeddings.create(model=self.model, input=text)
+        return response.data[0].embedding
+
+    def generate_embeddings_batch(self, texts: list[str]) -> list[list[float]]:
+        response = self.client.embeddings.create(model=self.model, input=texts)
+        return [item.embedding for item in response.data]
